ca8de2c86b56af538e2797bdbb0ab09f38056b9d
Scott King <jsking@andrew.cmu.edu>
2022-10-15 21:41:50 -0400

    Init xcode project

---
+++ Songbird/Songbird/SongbirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongbirdApp.swift
+//  Songbird
+//
+//  Created by Scott King on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SongbirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

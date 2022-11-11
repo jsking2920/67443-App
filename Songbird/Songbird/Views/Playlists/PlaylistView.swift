fa8133cdca6aae73355ecb7ca8e76eef8e215006
Scott King <jsking@andrew.cmu.edu>
2022-11-11 13:21:44 -0500

    Clean things up

--- Songbird/Songbird/Views/Playlists/PlaylistView.swift
+++ Songbird/Songbird/Views/Playlists/PlaylistView.swift
@@ -10,11 +10,10 @@ import Combine
 
 struct PlaylistView: View {
     
-    @EnvironmentObject var spotify: Spotify
-    @EnvironmentObject var userCollection: UserCollection
+	@EnvironmentObject var appState: AppState
     
     var body: some View {
         // View is just two date pickers and the list of songs in between those dates, and an export button leading to Spotify the App.
-        PlaylistDatePickerView(playlist: nil)
+			PlaylistDatePickerView(playlist: nil).environmentObject(appState)
     }
 }
--- Songbird/Songbird/Views/RootTabView.swift
+++ Songbird/Songbird/Views/RootTabView.swift
@@ -18,10 +18,15 @@ struct RootTabView: View {
 				Label("History", systemImage: "calendar")
 				Text("History")
 			}.environmentObject(appState).tag(1)
+			PlaylistView().tabItem{
+				Label("Playlists", systemImage: "music.note.list")
+				Text("Playlists")
+			}.environmentObject(appState).tag(2)
 			OptionsView().tabItem {
 				Label("Options", systemImage: "ellipsis")
 				Text("Options")
-			}.environmentObject(appState).tag(2)
+			}.environmentObject(appState).tag(3)
+			
 		}
 		.tabViewStyle(DefaultTabViewStyle())// PageTabViewStyle(indexDisplayMode: .always)  could be cool
 		.tint(.green) // todo: standardize this to spotify colors
